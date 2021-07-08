14f6c71e40be9b2550dd19f3a32273879719f46a
ramilfitkulin <56922122+RamZe3@users.noreply.github.com>
2021-07-08 23:25:20 +0400

    add base three-layer architecture

---
+++ Epam.BoardGamesForum/Epam.BoardGamesForum.ConsolePL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epam.BoardGamesForum.ConsolePL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Epam.BoardGamesForum/Epam.BoardGamesForum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epam.BoardGamesForum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
